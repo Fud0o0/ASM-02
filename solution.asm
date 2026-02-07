f0dcee3e70a5d0446eb2b9df88969f11d3909227
Fud0o0 <lukas.vaquette@oteria.fr>
2026-02-07 16:43:24 +0100

    création de de write_digit

--- solution.asm
+++ solution.asm
@@ -1,5 +1,24 @@
-global main
+section .text
+    global main
+
+write_digit:
+    push rbp
+    mov rbp, rsp
+    ; Erreur ici : on oublie add rdi, 48
+    push rdi            
+    mov rsi, rsp        
+    mov rax, 1          
+    mov rdi, 1          
+    mov rdx, 1          
+    syscall
+    pop rdi             
+    mov rsp, rbp
+    pop rbp
+    ret
+
 main:
     mov rdi, 9
     call write_digit
-    ret
+    mov rax, 60
+    xor rdi, rdi
+    syscall
