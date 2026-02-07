dd1b29ff646f8fb2f9e1b02f4f0620e68727ad34
Fud0o0 <lukas.vaquette@oteria.fr>
2026-02-07 16:51:51 +0100

    ajout de write_number mais soucis avec div jsp pk

--- solution.asm
+++ solution.asm
@@ -4,7 +4,7 @@ section .text
 write_digit:
     push rbp
     mov rbp, rsp
-    ; Erreur ici : on oublie add rdi, 48
+    add rdi, 48
     push rdi            
     mov rsi, rsp        
     mov rax, 1          
@@ -16,9 +16,31 @@ write_digit:
     pop rbp
     ret
 
-main:
-    mov rdi, 9
+write_number:
+    push rbp
+    mov rbp, rsp
+    mov rax, rdi
+    mov rcx, 0
+.loop:
+    mov rbx, 10
+    div rbx             
+    push rdx
+    inc rcx
+    cmp rax, 0
+    jne .loop
+.print:
+    pop rdi
+    push rcx
     call write_digit
+    pop rcx
+    loop .print
+    mov rsp, rbp
+    pop rbp
+    ret
+
+main:
+    mov rdi, 123
+    call write_number
     mov rax, 60
     xor rdi, rdi
     syscall
