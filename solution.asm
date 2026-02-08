e7992b24f96cd9f68e3f28e7768093ae8aa39d71
Fud0o0 <lukas.vaquette@oteria.fr>
2026-02-08 16:57:29 +0100

    ajout final normalement tous fonctionne de mon coter

--- solution.asm
+++ solution.asm
@@ -4,14 +4,14 @@ section .text
 write_digit:
     push rbp
     mov rbp, rsp
-    add rdi, 48
-    push rdi            
-    mov rsi, rsp        
-    mov rax, 1          
-    mov rdi, 1          
-    mov rdx, 1          
+    add rdi, 0x30
+    push rdi
+    mov rsi, rsp
+    mov rax, 1
+    mov rdi, 1
+    mov rdx, 1
     syscall
-    pop rdi             
+    pop rdi
     mov rsp, rbp
     pop rbp
     ret
@@ -21,25 +21,26 @@ write_number:
     mov rbp, rsp
     mov rax, rdi
     mov rcx, 0
-.loop:
+.split:
+    xor rdx, rdx
     mov rbx, 10
-    div rbx             
+    div rbx
     push rdx
     inc rcx
-    cmp rax, 0
-    jne .loop
-.print:
+    test rax, rax
+    jnz .split
+.display:
     pop rdi
     push rcx
     call write_digit
     pop rcx
-    loop .print
+    loop .display
     mov rsp, rbp
     pop rbp
     ret
 
 main:
-    mov rdi, 123
+    mov rdi, 12345
     call write_number
     mov rax, 60
     xor rdi, rdi
